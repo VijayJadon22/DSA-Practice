27c65f55d257ea52e34eae43fc04cbdaae7ed716
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-03-05 23:58:53 +0530

    Update App.java
    better approach for valid palindrome

--- App.java
+++ App.java
@@ -285,6 +285,24 @@ public int[] productExceptSelf(int[] nums) {
 // Valid palnidrome
 public boolean isPalindrome(String s) {
 
+
+    int i=0;
+        int j=s.length()-1;
+
+        while(i<j){
+            while(i<j && !Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+            }
+            while(i<j && !Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            }
+            if(Character.toLowerCase(s.charAt(i))!=Character.toLowerCase(s.charAt(j))){
+                return false;
+            }
+            i++; j--;
+        }
+        return true;
+
         int count[]=new int[26];
         StringBuilder sb=new StringBuilder();
         for(char c:s.toCharArray()){
