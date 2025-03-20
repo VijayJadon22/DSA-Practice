96467f4779de2e574fb1cb0c95ec4976488df966
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-03-20 21:52:59 +0530

    Implemented priorityQueue

--- MyQueue/Main.java
+++ MyQueue/Main.java
@@ -1,4 +1,5 @@
 package MyQueue;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,19 +17,33 @@ public class Main {
         // System.out.println(queue.dequeue());
         // System.out.println(queue.toString());
 
-        QueueUsingStack queue = new QueueUsingStack();
-        queue.enqueue(1);
-        queue.enqueue(2);
-        queue.enqueue(3);
-        queue.enqueue(4);
 
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
+        // // queue using stacks
+        // QueueUsingStack queue = new QueueUsingStack();
+        // queue.enqueue(1);
+        // queue.enqueue(2);
+        // queue.enqueue(3);
+        // queue.enqueue(4);
 
-        queue.enqueue(7);
-        queue.enqueue(8);
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
+        // System.out.println(queue.dequeue());
+        // System.out.println(queue.dequeue());
+
+        // queue.enqueue(7);
+        // queue.enqueue(8);
+        // System.out.println(queue.dequeue());
+        // System.out.println(queue.dequeue());
+        // System.out.println(queue.dequeue());
+
+        Queue<Integer> queue = new PriorityQueue<>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(-1);
+
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
     }
 }
