73d8e5dbee2d430620878c433764fec98ba793e2
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-03-20 23:22:28 +0530

    Implemented find first non-repeating character in a string with help of hashmap

--- RevisingHashMap/HashMapRevision.java
+++ RevisingHashMap/HashMapRevision.java
@@ -15,8 +15,7 @@ import java.util.*;
 public class HashMapRevision {
     public static void main(String[] args) {
 
-
-        ////basic map and its functions
+        //// basic map and its functions
         // Map<Integer, String> map = new HashMap<>();
         // map.put(1, "Piyush");
         // map.put(2, "Vijay");
@@ -27,8 +26,7 @@ public class HashMapRevision {
         // System.out.println(map.keySet());
         // System.out.println(map.size());
 
-
-        //created custom user class and stored user object as values in hashmap
+        // created custom user class and stored user object as values in hashmap
         // HashMap<Integer, User> map = new HashMap<>();
         // User u1 = new User(26, "Vijay");
         // User u2 = new User(20, "Aman");
@@ -36,7 +34,38 @@ public class HashMapRevision {
         // map.put(200, u2);
         // System.out.println(map.get(100).name);
 
+        // first non repeated character in a string str= "a green apple";
+        String str = "A Green Apple";
+        HashMap<Character, Integer> map = new HashMap<>();
 
+        // int count[] = new int[26];
+        // for (char ch : str.toCharArray()) {
+        //     if (!Character.isLetterOrDigit(ch))continue;
+        //     count[Character.toLowerCase(ch) - 'a']++;
+        // }
+
+        // for (int i = 0; i < 26; i++) {
+        //     if (count[i] == 1) {
+        //         System.out.println((char)('a'+i));
+        //     }
+        // }
+
+
+        for (char ch : str.toCharArray()) {
+            map.put(Character.toLowerCase(ch), map.getOrDefault(Character.toLowerCase(ch), 0) + 1);
+        }
+
+        System.out.println(map);
+
+        for (char ch : str.toCharArray()) {
+            if (map.get(Character.toLowerCase(ch))==1) {
+                System.out.println(ch);
+                break;
+            }
+        }
+
+        
+    
     }
 
 }
