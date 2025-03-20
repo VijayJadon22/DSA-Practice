b9916f058769b4c98af2d994dc9edcf3c83804fa
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-03-20 22:34:25 +0530

    Implemented Hashmap by custom classes

---
+++ RevisingHashMap/HashMapRevision.java
@@ -0,0 +1,42 @@
+package RevisingHashMap;
+
+import java.util.*;
+
+// class User {
+//     int rollno;
+//     String name;
+
+//     User(int num, String name) {
+//         this.rollno = rollno;
+//         this.name = name;
+//     }
+// }
+
+public class HashMapRevision {
+    public static void main(String[] args) {
+
+
+        ////basic map and its functions
+        // Map<Integer, String> map = new HashMap<>();
+        // map.put(1, "Piyush");
+        // map.put(2, "Vijay");
+        // System.out.println(map);
+        // System.out.println(map.get(2));
+        // System.out.println(map.containsKey(1));
+        // System.out.println(map.isEmpty());
+        // System.out.println(map.keySet());
+        // System.out.println(map.size());
+
+
+        //created custom user class and stored user object as values in hashmap
+        // HashMap<Integer, User> map = new HashMap<>();
+        // User u1 = new User(26, "Vijay");
+        // User u2 = new User(20, "Aman");
+        // map.put(100, u1);
+        // map.put(200, u2);
+        // System.out.println(map.get(100).name);
+
+
+    }
+
+}
